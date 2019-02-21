54931248cd359fb41e17017bd48f67ad7f0d5316
yuta <g1672028@tcu.ac.jp>
2019-02-21 13:48:23 +0900

    基本の流れの設定

--- js/ga.js
+++ js/ga.js
@@ -1,3 +1,66 @@
+//適応度計算
+function cal_fit()
+//順位による並べ替え
+function sort(int leftsta, int rightsta) {
+
+    var left = leftsta,
+        right = rightsta,
+        Gtmp;
+    var pivot, Ftmp;
+    var i;
+    var pivotindex = (left + right) / 2;
+
+    pivot = fit[pivotindex];
+
+    //クイックソート(昇順)
+    //*適応度が低いほど答えに近づく
+    while (1) {
+
+        while (fit[left] < pivot) {
+
+            //軸にあたるまで右側へ
+            left++;
+
+        }
+        while (fit[right] > pivot) {
+
+            //軸にあたるまで左側へ
+            right--;
+
+        }
+        if (left >= right) {
+
+            //軸が交差したら終了
+            break;
+
+        }
+
+        //入れ替え（適応度）
+        Ftmp = fit[left];
+        fit[left] = fit[right];
+        fit[right] = Ftmp;
+
+        //入れ替え（染色体）
+        for (i = 0; i < N; i++) {
+
+            Gtmp = indi[left][i];
+            indi[left][i] = indi[right][i];
+            indi[right][i] = Gtmp;
+
+        }
+
+        left++;
+        right--;
+    }
+}
+//親の選択
+function pare_choice(); 
+//交叉
+function cross(); 
+
+//突然変異
+function muta()//突然変異
+
 var chrom = 3; // 染色体数　回転角、左右,順位 出来れば次の手での撃破率を入れたい
 var herd = 50; //　個体数
 var elite = 2; //保存エリート数
@@ -21,19 +84,19 @@ while (cnt < gene) {
                 //ランダムに個体を生成する
                 indi[i][0] = Math.random() * spin;
                 indi[i][1] = Math.random() * move;
-                console.log(indi[i][0],indi[i][1]);
+                console.log(indi[i][0], indi[i][1]);
             }
         }
 
     if (cnt > 0) {
-        function pare_choice();//親の選択
-        function cross();//交叉
+        function pare_choice(); //親の選択
+        function cross(); //交叉
 
-        function muta();//突然変異
+        function muta(); //突然変異
     }
 
-    function cal_fit();//適応度計算
-    function sort(0,herd-1);//適応度順に並び変え
+    cal_fit(); //適応度計算
+    sort(0, herd - 1); //適応度順に並び変え
 
     cnt++;
 }
